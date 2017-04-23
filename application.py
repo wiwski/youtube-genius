e12b4c7670eeb6c329bec953d1e1afbbdabc546a
witold <witoldwrob@gmail.com>
2017-04-22 20:44:57 -0400

    Flow completed

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, render_template, redirect, request, flask
+from flask import Flask, session, render_template, redirect, request, url_for
 from flask_session import Session
 import httplib2
 from apiclient import discovery
@@ -46,12 +46,12 @@ class ContextTask(TaskBase):
 def index():
     return render_template('index.html')
 
-@app.route('/playlist')
-def sendPlaylist(maxLength=50):
+@app.route('/playlist/<pUrlId>')
+def sendPlaylist(pUrlId, maxLength=50):
     #playlist id
-    pUrl = urlparse(request.args.get('playlist_url'))
+    #pUrl = urlparse(request.args.get('playlist_url'))
     #TODO error handling when not playlist or wrong url
-    pUrlId = ''.join(urllib.parse.parse_qs(pUrl.query)['list'])
+    #pUrlId = ''.join(urllib.parse.parse_qs(pUrl.query)['list'])
 
     #get a the list of videos in playlist and extract the ids
     playlist = youtube.playlistItems().list(
@@ -239,14 +239,15 @@ def getRelatedHttp(videoId, maxResults=30, nextPageToken=None):
 
 @app.route('/createPlaylist', methods=['GET', 'POST', 'PUT'])
 def createPlaylist():
+    return render_template('mynewplaylist.html', newPlaylist='https://youtube.com/playlist?list=PLv2iKoUrni8OL93-7pvC_ZWiQfDVmCEnr')
     session['playlist'] = json.dumps(dict(request.form))
-    if 'credentials' not in flask.session:
-        flask.session['prev_page'] = 'createPlaylist'
-        return flask.redirect(flask.url_for('authentificated'))
-    credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
+    if 'credentials' not in session:
+        session['prev_page'] = 'createPlaylist'
+        return redirect(url_for('authentificated'))
+    credentials = client.OAuth2Credentials.from_json(session['credentials'])
     if credentials.access_token_expired:
-        flask.session['prev_page'] = 'createPlaylist'
-        return flask.redirect(flask.url_for('authentificated'))
+        session['prev_page'] = 'createPlaylist'
+        return redirect(url_for('authentificated'))
     playlist = json.loads(session.get('playlist'))
 
     http_auth = credentials.authorize(httplib2.Http())
@@ -273,48 +274,51 @@ def createPlaylist():
                 }
             }
         ).execute()
-        return 'https://youtube.com/playlist?list='+newPlaylisrt['id']
+    return render_template('mynewplaylist.html', newPlaylist='https://youtube.com/playlist?list='+newPlaylist['id'])
 
 @app.route('/authentificated')
 def authentificated():
     flow = client.flow_from_clientsecrets(
-        'secret.json',
+        '/home/witoldw/dev/youtube-genius/secret.json',
         scope=' https://www.googleapis.com/auth/youtube',
-        redirect_uri=flask.url_for('authentificated', _external=True)
+        redirect_uri=url_for('authentificated', _external=True)
     )
-    if 'code' not in flask.request.args:
+    if 'code' not in request.args:
         auth_uri = flow.step1_get_authorize_url()
-        return flask.redirect(auth_uri)
+        return redirect(auth_uri)
     else:
-        auth_code = flask.request.args.get('code')
+        auth_code = request.args.get('code')
         credentials = flow.step2_exchange(auth_code)
-        flask.session['credentials'] = credentials.to_json()
-        return flask.redirect(flask.url_for(flask.session['prev_page']))
+        session['credentials'] = credentials.to_json()
+        return redirect(url_for(session['prev_page']))
 
 @app.route('/user/playlists')
 def user():
-    if 'credentials' not in flask.session:
-        return flask.redirect(flask.url_for('authentificated', prev_page='user'))
-    credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
+    if 'credentials' not in session:
+        session['prev_page'] = 'user'
+        return redirect(url_for('authentificated', prev_page='user'))
+    credentials = client.OAuth2Credentials.from_json(session['credentials'])
     if credentials.access_token_expired:
-        flask.session['prev_page'] = 'user'
-        return flask.redirect(flask.url_for('authentificated'))
+        session['prev_page'] = 'user'
+        return redirect(url_for('authentificated'))
     else:
         http_auth = credentials.authorize(httplib2.Http())
         youtube = discovery.build('youtube', 'v3', http=http_auth)
         allPlaylists = youtube.playlists().list(
-            part="snippet",
-            mine=True
+            part="snippet,id",
+            mine=True,
+            maxResults=50
         ).execute()
         playlists = []
         for p in allPlaylists['items']:
-            p = p['snippet']
+            snippet = p['snippet']
             playlist = {}
-            playlist['thumbnail'] = p['thumbnails']['high']['url']
-            playlist['description'] = p['localized']['description']
-            playlist['title'] = p['localized']['title']
+            playlist['thumbnail'] = snippet['thumbnails']['high']['url']
+            playlist['description'] = snippet['localized']['description']
+            playlist['title'] = snippet['localized']['title']
+            playlist['id'] = p['id']
             playlists.append(playlist)
-            return render_template('playlists.html', playlists=playlists)
+        return render_template('playlists.html', playlists=playlists)
 
 
 
